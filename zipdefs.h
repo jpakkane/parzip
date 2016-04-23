353d207a890a813c0e62b87036b7ebb97021f9e0
Jussi Pakkanen <jpakkane@gmail.com>
2016-04-23 21:59:16 +0300

    Can read entries.

--- zipdefs.h
+++ zipdefs.h
@@ -16,17 +16,21 @@
 
 #pragma once
 
-#include"zipdefs.h"
+#include<cstdint>
 #include<string>
-#include<vector>
 
-class ZipFile {
-
-public:
-    ZipFile(const char *fname);
-
-private:
-
+struct localheader {
+    uint32_t signature;
+    uint16_t needed_version;
+    uint16_t gp_bitflag;
+    uint16_t compression;
+    uint16_t last_mod_time;
+    uint16_t last_mod_date;
+    uint8_t crc32[4];
+    uint32_t compressed_size;
+    uint32_t uncompressed_size;
+    uint16_t file_name_length;
+    uint16_t extra_field_length;
     std::string fname;
-    std::vector<localheader> entries;
+    std::string extra;
 };
--- zipfile.cpp
+++ zipfile.cpp
@@ -16,18 +16,54 @@
 
 
 #include"zipfile.h"
-#include<memory>
 #include<cstdio>
 #include<stdexcept>
+#include<memory>
+
+const constexpr int LOCAL_SIG = 0x04034b50;
+
+namespace {
+
+localheader read_local_entry(FILE *f) {
+    localheader h;
+    h.signature = LOCAL_SIG;
+    fread(&h.needed_version, sizeof(uint16_t), 1, f);
+    fread(&h.gp_bitflag, sizeof(uint16_t), 1, f);
+    fread(&h.compression, sizeof(uint16_t), 1, f);
+    fread(&h.last_mod_time, sizeof(uint16_t), 1, f);
+    fread(&h.last_mod_date, sizeof(uint16_t), 1, f);
+    fread(&h.crc32, 4, 1, f);
+    fread(&h.compressed_size, sizeof(uint32_t), 1, f);
+    fread(&h.uncompressed_size, sizeof(uint32_t), 1, f);
+    fread(&h.file_name_length, sizeof(uint16_t), 1, f);
+    fread(&h.extra_field_length, sizeof(uint16_t), 1, f);
+    h.fname.insert(0, h.file_name_length, 'a');
+    h.extra.insert(0, h.extra_field_length, 'b');
+    fread(&(h.fname[0]), h.file_name_length, 1, f);
+    fread(&(h.extra[0]), h.extra_field_length, 1, f);
+    return h;
+}
+
+}
 
 ZipFile::ZipFile(const char *fname) {
     std::unique_ptr<FILE, int(*)(FILE *f)> ifile(fopen(fname, "r"), fclose);
     if(!ifile) {
         throw std::runtime_error("Could not open input file.");
     }
-    char header[2];
-    fread(header, 2, 1, ifile.get());
-    if(header[0] != 'P' || header[1] != 'K') {
-        throw std::runtime_error("Not a zip file.");
+    while(true) {
+        auto curloc = ftell(ifile.get());
+        uint32_t head;
+        fread(&head, sizeof(uint32_t), 1, ifile.get());
+        if(head != LOCAL_SIG) {
+            fseek(ifile.get(), curloc, SEEK_SET);
+            break;
+        }
+        entries.push_back(read_local_entry(ifile.get()));
+        fseek(ifile.get(), entries.back().compressed_size, SEEK_CUR);
+        if(entries.back().gp_bitflag & (1<<2)) {
+            fseek(ifile.get(), 3*4, SEEK_CUR);
+        }
     }
+    printf("This file has %d entries.\n", (int)entries.size());
 }
