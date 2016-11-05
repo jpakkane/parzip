3d81e0b9e41250e34450ed529ff4c397895bcef5
Jussi Pakkanen <jpakkane@gmail.com>
2016-11-06 01:22:55 +0200

    Created win32 app skeleton.

---
+++ windows/parzip-win.cpp
@@ -0,0 +1,70 @@
+#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR, int iCmdShow) {
+    static TCHAR name[] = TEXT("Parzip");
+    HWND hwnd;
+    MSG msg;
+    WNDCLASS wndclass;
+
+    wndclass.style = CS_HREDRAW | CS_VREDRAW;
+    wndclass.lpfnWndProc = WndProc;
+    wndclass.cbClsExtra = 0;
+    wndclass.cbWndExtra = 0;
+    wndclass.hInstance = hInstance;
+    wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndclass.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH);
+    wndclass.lpszMenuName = NULL;
+    wndclass.lpszClassName = name;
+
+    if(!RegisterClass(&wndclass)) {
+      MessageBox(NULL, TEXT("Could not initialize wndclass"), name, MB_ICONERROR);
+      return 0;
+    }
+
+    hwnd = CreateWindow(name,
+                        TEXT("Parallel unzipper."),
+                        WS_OVERLAPPEDWINDOW,
+                        CW_USEDEFAULT,
+                        CW_USEDEFAULT,
+                        CW_USEDEFAULT,
+                        CW_USEDEFAULT,
+                        NULL,
+                        NULL,
+                        hInstance,
+                        NULL);
+
+    ShowWindow(hwnd, iCmdShow);
+    UpdateWindow(hwnd);
+    while(GetMessage(&msg, NULL, 0, 0)) {
+      TranslateMessage(&msg);
+      DispatchMessage(&msg);
+    }
+    return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam) {
+  HDC hdc;
+  PAINTSTRUCT ps;
+  RECT rect;
+
+  switch(message) {
+  case WM_CREATE: return 0;
+
+  case WM_PAINT:
+    hdc = BeginPaint(hwnd, &ps);
+    GetClientRect(hwnd, &rect);
+    DrawText(hdc, TEXT("Add treeview here."), -1, &rect,
+             DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+    EndPaint(hwnd, &ps);
+    return 0;
+
+  case WM_DESTROY:
+    PostQuitMessage(0);
+    return 0;
+  }
+
+  return DefWindowProc(hwnd, message, wparam, lparam);
+}
